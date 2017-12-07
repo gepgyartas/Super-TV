4f51978d6f2bedf340e8f62631eb246bf8066e61
Laci <laci@Lacis-MacBook-Pro.local>
2017-12-07 14:05:15 +0100

    Initial Commit

---
+++ Super TV/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Super TV
+//
+//  Created by Laci on 2017. 12. 07..
+//  Copyright © 2017. Illés László. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
